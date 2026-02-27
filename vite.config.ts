248e4bafc6f12b889ccd3ea359327e2ef584122b
victormutune <victormutune254@gmail.com>
2026-02-27 20:56:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', 
   plugins: [react()],
-  test: {
-    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-  }
 })
